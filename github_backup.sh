60564018b38a8683602a5bc2e7e014ea55b3aa5d
Rashmika Prabhath <rashmikade122@gmail.com>
2026-03-18 17:22:42 +0530

    Daily backup Wed Mar 18 17:22:42 +0530 2026

--- github_backup.sh
+++ github_backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Copy latest cowrie log
-cp /home/cowrie/cowrie/var/log/cowrie/cowrie.json /home/cowrie/cowrie-analysis/cowrie_$(date +\%F_\%H).json
-
 # Go to repo
 cd /home/cowrie/cowrie-analysis
 
-# Push to GitHub
-git add .
-git commit -m "Auto backup $(date)"
-git push origin main
+# Create daily backup ONLY if not exists
+FILE="cowrie_$(date +%F).json"
+
+if [ ! -f "$FILE" ]; then
+  cp /home/cowrie/cowrie/var/log/cowrie/cowrie.json "$FILE"
+
+  git add .
+  git commit -m "Daily backup $(date)"
+  git push origin main
+fi
